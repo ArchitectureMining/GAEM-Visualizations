b132dc91482afbe9af0b9f3ced6c92b2ead6db41
Thomas Ipskamp <thomas.ipskamp@gmail.com>
2017-11-26 18:08:11 +0100

    Changed the neo4j datamodel to store only 2 arrays
    All other getters are deduced from the data in those 2 arrays.

--- src/model/ColumnData.ts
+++ src/model/ColumnData.ts
@@ -1,25 +1,196 @@
-export interface ColumnData {
-	fullname?: string;
-	totalCallsToThis: number;
-	totalCallsFromThis: number;
-	totalCalls: number;
-	subElements: SubElement[];
+// look into structure with element1, element2, number1->2, number2->1
+// maybe even just target, numberTo, numberFrom?
+export class AggregateCall {
+	constructor(
+		public from: string,
+		public to: string,
+		public totalCalls: number
+	) {
+
+	}
 }
 
-export interface SubElement {
-	name: string;
-	fullname: string;
-	calls: {
-		to: AggregateCall[],
-		from: AggregateCall[]
-	};
-	totalCallsToThis: number;
-	totalCallsFromThis: number;
-	totalCalls: number;
+export class SubElement {
+	constructor(
+		public column: ColumnData,
+		public fullname: string
+	) {
+
+	}
+
+	get name(): string {
+		return this.fullname.split(".").pop();
+	}
+
+	get calls(): AggregateCall[] {
+		return this.column.calls.filter((call) => {
+			return (call.from === this.fullname || call.to === this.fullname);
+		});
+	}
+
+	get internalCalls(): AggregateCall[] {
+		return this.calls.filter((call) => {
+			return (this.column.subElementFullnames.some((value) => {
+				return call.from === value;
+			}) && this.column.subElementFullnames.some((value) => {
+				return call.to === value;
+			}));
+		});
+	}
+
+	get totalInternalCalls(): number {
+		return this.internalCalls.reduce((total, current) => {
+			return total + current.totalCalls;
+		}, 0);
+	}
+
+	get totalInternalCallsToThisElement(): number {
+		return this.internalCalls.reduce((total, call) => {
+			if (call.to === this.fullname) {
+				return total + call.totalCalls;
+			} else {
+				return total;
+			}
+		}, 0);
+	}
+
+	get totalInternalCallsFromThisElement(): number {
+		return this.internalCalls.reduce((total, call) => {
+			if (call.from === this.fullname) {
+				return total + call.totalCalls;
+			} else {
+				return total;
+			}
+		}, 0);
+	}
+
+	get externalCalls(): AggregateCall[] {
+		return this.calls.filter((call) => {
+			return !(this.column.subElementFullnames.some((value) => {
+				return call.from === value;
+			}) && this.column.subElementFullnames.some((value) => {
+				return call.to === value;
+			}));
+		});
+	}
+
+	get totalExternalCalls(): number {
+		return this.externalCalls.reduce((total, current) => {
+			return total + current.totalCalls;
+		}, 0);
+	}
+
+	get totalExternalCallsToThisElement(): number {
+		return this.externalCalls.reduce((total, call) => {
+			if (call.to === this.fullname) {
+				return total + call.totalCalls;
+			} else {
+				return total;
+			}
+		}, 0);
+	}
+
+	get totalExternalCallsFromThisElement(): number {
+		return this.externalCalls.reduce((total, call) => {
+			if (call.from === this.fullname) {
+				return total + call.totalCalls;
+			} else {
+				return total;
+			}
+		}, 0);
+	}
+
+	get totalCallsToThisElement(): number {
+		return this.calls.reduce((total, call) => {
+			if (call.to === this.fullname) {
+				return total + call.totalCalls;
+			} else {
+				return total;
+			}
+		}, 0);
+	}
+
+	get totalCallsFromThisElement(): number {
+		return this.calls.reduce((total, call) => {
+			if (call.from === this.fullname) {
+				return total + call.totalCalls;
+			} else {
+				return total;
+			}
+		}, 0);
+	}
+
+	get totalCalls(): number {
+		return this.calls.reduce((total, call) => {
+			return total + call.totalCalls;
+		}, 0);
+	}
 }
 
-export interface AggregateCall {
-	from: string;
-	to: string;
-	numberOfCalls: number;
+// columnData fullname has no use up until now, so is excluded on purpose.
+export class ColumnData {
+	public subElements: SubElement[] = [];
+	public calls: AggregateCall[] = [];
+
+	get subElementFullnames(): string[] {
+		return this.subElements.map((element) => {
+			return element.fullname;
+		});
+	}
+
+	get internalCalls(): AggregateCall[] {
+		return this.subElements.reduce((total, current) => {
+			return total.concat(current.internalCalls);
+		}, new Array<AggregateCall>());
+	}
+
+	// internal calls to/from this = total internal calls
+
+	get totalInternalCalls(): number {
+		return this.subElements.reduce((total, current) => {
+			return total + current.totalInternalCalls;
+		}, 0);
+	}
+
+	get externalCalls(): AggregateCall[] {
+		return this.subElements.reduce((total, current) => {
+			return total.concat(current.externalCalls);
+		}, new Array<AggregateCall>());
+	}
+
+	get totalExternalCallsToThisColumn(): number {
+		return this.subElements.reduce((total, current) => {
+			return total + current.totalExternalCallsToThisElement;
+		}, 0);
+	}
+
+	get totalExternalCallsFromThisColumn(): number {
+		return this.subElements.reduce((total, current) => {
+			return total + current.totalExternalCallsFromThisElement;
+		}, 0);
+	}
+
+	get totalExternalCalls(): number {
+		return this.subElements.reduce((total, current) => {
+			return total + current.totalExternalCalls;
+		}, 0);
+	}
+
+	get totalCallsToThisColumn(): number {
+		return this.subElements.reduce((total, current) => {
+			return total + current.totalCallsToThisElement;
+		}, 0);
+	}
+
+	get totalCallsFromThisColumn(): number {
+		return this.subElements.reduce((total, current) => {
+			return total + current.totalCallsFromThisElement;
+		}, 0);
+	}
+
+	get totalCalls(): number {
+		return this.totalCallsToThisColumn + this.totalCallsFromThisColumn;
+	}
+
+	// optional: add internal/external accessors as well?
 }
--- src/model/DataModel.ts
+++ src/model/DataModel.ts
@@ -1,20 +1,9 @@
+import { ColumnData } from "./ColumnData";
+
 export interface DataModel {
 	connect(): void;
-	getColumnData(elementType: string, fullname: string, runs: string[]);
+	getColumnData(elementType: string, fullname: string, runs: string[]): Promise<ColumnData>;
 	// return promise? so we can wait on multiple calls before rendering
 	// return [ComponentData[]]? 1 per column?
 	disconnect(): void;
 }
-
-/*export interface ComponentData {
-	fullname: string;
-	incoming: number;
-	outgoing: number;
-	calls: CallData[];
-}
-
-export interface CallData {
-	fullname: string;
-	from: number;
-	to: number;
-}*/
